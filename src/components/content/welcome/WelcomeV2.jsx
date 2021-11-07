b2038671a88e96e99ee60fc3072c392a74169755
Kamil Rojek <kamroj@gmail.com>
2021-11-07 14:30:33 +0100

    Add calculation of button circ button position

--- src/components/content/welcome/WelcomeV2.jsx
+++ src/components/content/welcome/WelcomeV2.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { scroller } from "react-scroll";
+import { Link, scroller } from "react-scroll";
 import Video from "../../../assets/video/beer.webm";
 import { Key } from "../../../Keys";
-import { ElementNames, ScrollerProp } from "../../navbar/NavbarData";
+import { ElementNames, NavbarData, ScrollerProp } from "../../navbar/NavbarData";
 import "./WelcomeV2.scss";
 
 export default function WelcomeV2() {
@@ -23,20 +23,46 @@ export default function WelcomeV2() {
     );
   }
 
+  function calculatePointOnCircle(radius, angle, cx, cy) {
+    angle *= Math.PI / 180;
+    let x = cx + radius * Math.cos(angle);
+    let y = cy + radius * Math.sin(angle);
+    return { x: x, y: y };
+  }
+
+  function decorateDataWithPointsPositionOnCircle(data, radius, cx, cy) {
+    console.log(data)
+    let decoratedData = []
+    let angleIncremental = 360 / data.length;
+    let angle = 0;
+
+    for (let i = 0; i < data.length; i++) {
+      let point = calculatePointOnCircle(radius, angle, cx, cy);
+      angle += angleIncremental;
+      decoratedData.push({ ...data[i], ...point });
+    }
+
+    return decoratedData;
+  }
+
   return (
     <section className="welcome-section">
       <video id="welcome-background-video" muted={true} autoPlay loop>
         <source src={Video} type="video/webm" />
       </video>
       <div className="welcome-content-container">
-
-
         <div className="welcome-menu-container">
-          <div className="welcome-menu-top-container">
-            <div className="welcome-menu-element-container welcome-menu-element-btn-top-side welcome-menu-element-btn-left-side">
-              <button>{t(Key.Gallery)}</button>
-            </div>
-            <div className="welcome-menu-element-container welcome-menu-element-btn-center-side">
+          {decorateDataWithPointsPositionOnCircle(NavbarData, 35, 50, 50).map((item, index) => {
+            return (
+              <Link key={index} activeClass="active" smooth={ScrollerProp.smooth} to={item.element} duration={ScrollerProp.duration}>
+                <button key={index} className="welcome-menu-btn" style={{width:"14vw", height: "6vh", left: `calc(${item.x}vw - 14vw / 2)`, top: `calc(${item.y}vh - 6vh / 2)`}}>
+                  {t(item.title).toUpperCase()}
+                </button>
+              </Link>
+            );
+          })}
+          {/* <div className="welcome-menu-top-container"> */}
+          {/* <div className="welcome-menu-element-container welcome-menu-element-btn-center-side">
               <button>{t(Key.Reservation)}</button>
             </div>
             <div className="welcome-menu-element-container welcome-menu-element-btn-top-side welcome-menu-element-btn-right-side">
@@ -52,11 +78,11 @@ export default function WelcomeV2() {
             </div>
             <div className="welcome-menu-element-container welcome-menu-element-btn-bottom-side welcome-menu-element-btn-right-side">
               <button>{t(Key.Contact)}</button>
-            </div>
-          </div>
+            </div> */}
+          {/* </div> */}
         </div>
 
-        {/* <div className="welcome-baron-logo-conteiner">
+        <div className="welcome-baron-logo-conteiner">
           <button
             className="welcome-baron-logo"
             onClick={() => scroller.scrollTo(ElementNames.aboutUs, { smooth: ScrollerProp.smooth, duration: ScrollerProp.duration })}
@@ -71,7 +97,7 @@ export default function WelcomeV2() {
           <span className="welcome-baron-logo-desc">
             {colorFirstLetter(t("logo_description_consols_key"))} {t("logo_description_and_key")} {colorFirstLetter(t("logo_description_board_games_key"))}
           </span>
-        </div> */}
+        </div>
       </div>
     </section>
   );
