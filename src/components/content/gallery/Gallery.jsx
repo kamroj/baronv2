f3c5130b6d29bc879b09d4f46a9be2fcf3ffb893
Kamil Rojek <kamroj@gmail.com>
2021-11-22 22:14:15 +0100

    Fix gallery translation

--- src/components/content/gallery/Gallery.jsx
+++ src/components/content/gallery/Gallery.jsx
@@ -8,13 +8,14 @@ import { BaronImageUrls } from "./ImgUlrs";
 import { Trans, useTranslation } from "react-i18next";
 
 export default function Gallery() {
-  
+  useTranslation()
+
   return (
     <Element name={ElementNames.gallery}>
       <div className="gallery-container">
         <div className="gallery-header-container">
           <Trans i18nKey="gallery_header_key" components={{ baron: <span style={{ color: "rgb(0, 144, 0)" }} /> }} />
-        </div>
+          </div>
         <div className="gallery-container-outer-lines">
           <div className="gallery-image-gallery">
             <ReactImageGallery autoPlay={true} slideInterval={5000} items={BaronImageUrls} />
