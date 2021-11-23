02bfadd3b90cf91317ca7be3559554c5e2cadd0e
Kamil Rojek <kamroj@gmail.com>
2021-11-23 12:49:41 +0100

    Messanger impl

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import AboutUs from "./components/content/aboutus/AboutUs";
 import { Element } from "react-scroll";
 import Welcome from "./components/content/welcome/Welcome";
 import Gallery from "./components/content/gallery/Gallery";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
   return (
     <div>
       <Element name="top-element" />
       <Welcome />
+      
       <div>
         <Navbar />
         <AboutUs />
         <Gallery />
       </div>
+      <MessengerCustomerChat pageId="229571084064511" appId="1520264291685156" />
     </div>
   );
 }
