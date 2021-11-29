9c7346276d62b878f7089b578bc2c8802c212e9f
Kamil Rojek <kamroj@gmail.com>
2021-11-29 14:51:49 +0100

    Remove no needed stuff

--- src/App.js
+++ src/App.js
@@ -2,25 +2,11 @@ import React from "react";
 
 import "./App.scss";
 import LoadingPage from "./components/loadingpage/LoadingPage";
-import useWindowDimensions from "./hooks/useWindowDimensionsHook";
 
 function App() {
-  const { windowWidth } = useWindowDimensions();
-  const isMobile = (mobileWidth = 650) => windowWidth < mobileWidth;
-
   return (
     <div>
       <LoadingPage />
-      {/* <Element name="top-element" /> */}
-      {/* <Welcome /> */}
-      {/* <div> */}
-      {/* <Navbar /> */}
-      {/* <AboutUs /> */}
-      {/* <Gallery /> */}
-      {/* <Reservation /> */}
-      {/* <Promotion /> */}
-      {/* </div> */}
-      {/* <MessengerCustomerChat pageId="229571084064511" appId="1520264291685156" /> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import LanguageDetecotr from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
-import 'bootstrap/dist/js/bootstrap.js'
 
 import './index.scss';
 import App from './App';
-
-i18n
-  .use(initReactI18next) 
-  .use(LanguageDetecotr)
-  .use(HttpApi)
-  .init({
-    fallbackLng: "gb",
-    detection: {
-      order: ['cookie', 'localStorage', 'path', 'subdomain', 'htmlTag'],
-      caches: ['cookie']
-    },
-    backend: {
-      loadPath: '/assets/locales/{{lng}}/translation.json',
-    },
-    react: { useSuspense: false }
-  });
 
 ReactDOM.render(
   <React.StrictMode>
