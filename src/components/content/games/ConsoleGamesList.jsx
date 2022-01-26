5d6d381ba62e0d78b03c667a3234bb2b770ce76e
Kamil Rojek <kamroj@gmail.com>
2022-01-26 20:41:27 +0100

    Adjust game for mobiles

--- src/components/content/games/ConsoleGamesList.jsx
+++ src/components/content/games/ConsoleGamesList.jsx
@@ -5,8 +5,33 @@ export const ConsoleGamesList = [
     yt: "https://www.youtube.com/embed/RqskWK_mj1E"
   },
   {
-    title: "MORTAL COMBAT X",
-    img: "https://smartcdkeys.com/image/cachewebp/data/products/mortal-kombat-x-xbox-one-key/cover/mortal-kombat-x-xbox-one-cover-390x580.webp",
+    title: "INJUSTICE 2",
+    img: "https://i.postimg.cc/2yN7YJK4/i-injustice-2-edycja-legendarna-gra-xone.jpg",
+    yt: "https://www.youtube.com/embed/RqskWK_mj1E"
+  },
+  {
+    title: "TEKKEN 7",
+    img: "https://i.postimg.cc/hP1WkR51/tekken-7-xbox-one-xbox-one-game-microsoft-store-europe-cover.jpg",
+    yt: "https://www.youtube.com/embed/RqskWK_mj1E"
+  },
+  {
+    title: "TEKKEN TOURNAMENT 2",
+    img: "https://i.postimg.cc/Y2ymQT38/6076b42a7e696c1c077e3832.jpg",
+    yt: "https://www.youtube.com/embed/RqskWK_mj1E"
+  },
+  {
+    title: "RAYMAN LEGENDS",
+    img: "https://i.postimg.cc/T1kjbwPc/0-0-product-Gfx-dd5f61759f59d61266ce7e642189dc82.jpg",
+    yt: "https://www.youtube.com/embed/RqskWK_mj1E"
+  },
+  {
+    title: "FORZA 7",
+    img: "https://i.postimg.cc/0yCSKjHK/e5aad899-fm7.jpg",
+    yt: "https://www.youtube.com/embed/RqskWK_mj1E"
+  },
+  {
+    title: "MORTAL COMBAT 11",
+    img: "https://i.postimg.cc/BQr40CSJ/i-mortal-kombat-11-gra-xbox-one.jpg",
     yt: "https://www.youtube.com/embed/jSi2LDkyKmI"
   }
 ];
