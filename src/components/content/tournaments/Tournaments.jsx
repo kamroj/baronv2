22bf601cba34251d2232b85e9ca40fcc29640b03
Kamil Rojek <kamroj@gmail.com>
2022-01-31 20:04:18 +0100

    Add translation, tournament btn

--- src/components/content/tournaments/Tournaments.jsx
+++ src/components/content/tournaments/Tournaments.jsx
@@ -1,19 +1,26 @@
 import { Parallax } from "react-parallax";
+import { Element } from "react-scroll";
+import { ElementNames } from "../../navbar/NavbarData";
+import { Trans } from "react-i18next";
+
 import "./Tournaments.scss";
+import { useTranslation } from "react-i18next";
 
 export default function Tournaments() {
+  useTranslation();
+
   return (
-    <Parallax bgImage="https://i.postimg.cc/fLDNf0N8/testimonials.jpg" strength={500}>
-      <div className="tournaments-main-container">
-        <div className="tournaments-description">
-          <p>
-            Lubisz rywalizację? <br />
-            Weź udział w naszych TURNIEJACH! <br />
-            Dla większych grup lub firm istnieje możliwość organizacji specjalnych turniejów! <br />
-            Przejdz do sekcji kontakty aby skontaktować się z nami.
-          </p>
+    <Element name={ElementNames.tournaments}>
+      <Parallax bgImage="https://i.postimg.cc/fLDNf0N8/testimonials.jpg" strength={300} blur={1}>
+        <div className="tournaments-main-container">
+          <div className="tournaments-description">
+            <Trans i18nKey="tournaments_desc_key" />
+          </div>
+          <a className="tournaments-events-btn" href="https://www.facebook.com/baronkrakow/events" target="_blank" rel="noreferrer noopener">
+            <Trans i18nKey="tournaments_btn_desc_key" />
+          </a>
         </div>
-      </div>
-    </Parallax>
+      </Parallax>
+    </Element>
   );
 }
