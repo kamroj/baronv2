634f07dcce1d7feac7e236a48d19e7c4df54fa3f
Kamil Rojek <kamroj@gmail.com>
2022-01-31 20:29:12 +0100

    Adjust mobilie view

--- src/components/content/tournaments/Tournaments.jsx
+++ src/components/content/tournaments/Tournaments.jsx
@@ -14,7 +14,10 @@ export default function Tournaments() {
       <Parallax bgImage="https://i.postimg.cc/fLDNf0N8/testimonials.jpg" strength={300} blur={1}>
         <div className="tournaments-main-container">
           <div className="tournaments-description">
-            <Trans i18nKey="tournaments_desc_key" />
+            <Trans i18nKey="tournaments_desc_key" components={{ color: <span style={{ color: "#bafc00", whiteSpace:"nowrap" }} /> }}/>
+            <div className="tournaments-description-contact">
+              <Trans i18nKey="tournaments_desc_contact_key" />
+            </div>
           </div>
           <a className="tournaments-events-btn" href="https://www.facebook.com/baronkrakow/events" target="_blank" rel="noreferrer noopener">
             <Trans i18nKey="tournaments_btn_desc_key" />
