4bd9408888db196da876794544e7018c1ace83b8
Kamil Rojek <kamroj@gmail.com>
2022-02-07 22:07:07 +0100

    Add board games, fix displaying items in list viewer

--- src/components/content/games/BoardGamesList.jsx
+++ src/components/content/games/BoardGamesList.jsx
@@ -30,13 +30,63 @@ export const BoardGamesList = [
     yt: "https://www.youtube.com/embed/RqskWK_mj1E"
   },
   {
+    title: "CZÓŁKO BEZ CENZURY",
+    img: "https://i.postimg.cc/fRFpgTzN/czolko.png",
+    yt: "https://www.youtube.com/embed/RqskWK_mj1E"
+  },
+  {
     title: "COATL",
     img: "https://i.postimg.cc/PxXpf68H/coatl.png",
     yt: "https://www.youtube.com/embed/RqskWK_mj1E"
   },
   {
+    title: "T.I.M.E STORIES",
+    img: "https://i.postimg.cc/vBPW3Dy7/837740-1024-0-1-t-i-m-e-stories-rebel.png",
+    yt: "https://www.youtube.com/embed/RqskWK_mj1E"
+  },
+  {
+    title: "TRIOMINOS",
+    img: "https://i.postimg.cc/zfT81PG3/Goliath-Gra-rodzinna-Triominos-Classic.png",
+    yt: "https://www.youtube.com/embed/RqskWK_mj1E"
+  },
+  {
     title: "DREADFUL CIRCUS",
     img: "https://i.postimg.cc/c4z80Mmg/dreadful-circus-port.png",
+    yt: "https://www.youtube.com/embed/RqskWK_mj1E"
+  },
+  {
+    title: "SMALLWORLD",
+    img: "https://i.postimg.cc/FK2t1b18/0824968790021.png",
+    yt: "https://www.youtube.com/embed/RqskWK_mj1E"
+  },
+  {
+    title: "SENTIENT",
+    img: "https://i.postimg.cc/mrSMgjd2/sentient-3d-lores-small.png",
+    yt: "https://www.youtube.com/embed/RqskWK_mj1E"
+  },
+  {
+    title: "RUMMIKUB",
+    img: "https://i.postimg.cc/15pYKhXW/tmtoys-gra-strategiczna-rummikub-standard-b-iext104301945.png",
+    yt: "https://www.youtube.com/embed/RqskWK_mj1E"
+  },
+  {
+    title: "BLOODBORNE",
+    img: "https://i.postimg.cc/8CWF7Htr/bloodborne-gp-3d-box.png",
+    yt: "https://www.youtube.com/embed/RqskWK_mj1E"
+  },
+  {
+    title: "POTWORY W TOKIO",
+    img: "https://i.postimg.cc/DzgTp4zM/5902560384079.png",
+    yt: "https://www.youtube.com/embed/RqskWK_mj1E"
+  },
+  {
+    title: "CYWILIZACJA - POPRZEZ WIEKI",
+    img: "https://i.postimg.cc/wvYcb37n/866489-0-i1064.png",
+    yt: "https://www.youtube.com/embed/RqskWK_mj1E"
+  },
+  {
+    title: "SPISKOWCY",
+    img: "https://i.postimg.cc/SsZRYvFV/i-portal-games-spiskowcy.png",
     yt: "https://www.youtube.com/embed/RqskWK_mj1E"
   },
   {
