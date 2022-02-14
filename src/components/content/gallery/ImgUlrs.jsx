4185d2d7961123a0c3e55eeae770b95039d6a2a1
Kamil Rojek <kamroj@gmail.com>
2022-02-14 16:37:15 +0100

    Update gallery images

--- src/components/content/gallery/ImgUlrs.jsx
+++ src/components/content/gallery/ImgUlrs.jsx
@@ -1,12 +1,12 @@
 export const BaronImageUrls = [
-  { original: "https://i.postimg.cc/0yF5T8C5/IMG-0692.jpg" },
-  { original: "https://i.postimg.cc/BnXQ78Hw/IMG-0694.jpg" },
-  { original: "https://i.postimg.cc/HLMWHfSw/IMG-0696.jpg" },
-  { original: "https://i.postimg.cc/wTxBJ4vz/IMG-0698.jpg" },
-  { original: "https://i.postimg.cc/RhSv7qvd/IMG-0711.jpg" },
   { original: "https://i.postimg.cc/3NtYn7RT/IMG-0713.jpg" },
   { original: "https://i.postimg.cc/wTWjk5ym/IMG-0737.jpg" },
   { original: "https://i.postimg.cc/85VpCt3q/PXL-20220117-213342950-PORTRAIT.jpg" },
   { original: "https://i.postimg.cc/L5D6wLxG/PXL-20220117-213521370-PORTRAIT-2.jpg" },
   { original: "https://i.postimg.cc/FzNHTDVj/PXL-20220117-215321493-PORTRAIT.jpg" },
+  { original: "https://i.postimg.cc/fbD442zh/IMG-1603.jpg" },
+  { original: "https://i.postimg.cc/9MQv0Dw8/IMG-1605.jpg" },
+  { original: "https://i.postimg.cc/VvSp92RX/IMG-1768.jpg" },
+  { original: "https://i.postimg.cc/rpJBq1sR/IMG-1839.jpg" },
+  { original: "https://i.postimg.cc/DZpRX7q2/IMG-1867.jpg" },
 ];
